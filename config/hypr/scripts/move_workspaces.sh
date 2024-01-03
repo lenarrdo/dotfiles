c8f237f57915118c7ad2f1d98d03d5fcf1fda977
leonardo <leonardo.miani23@gmail.com>
2024-01-03 15:34:33 +0100

    added hyprland config

---
+++ config/hypr/scripts/move_workspaces.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "moving workspace 1 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "1 1"
+
+echo "moving workspace 2 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "2 1"
+
+echo "moving workspace 3 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "3 1"
+
+echo "moving workspace 4 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "4 1"
+
+echo "moving workspace 5 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "5 1"
+
+echo "moving workspace 6 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "6 1"
+
+echo "moving workspace 7 to external monitor..."
+hyprctl dispatch moveworkspacetomonitor "7 1"
